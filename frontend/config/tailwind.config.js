befe03efa60618bc18d1fcb09ab37e0046c6f4e8
Tuong Vy <tuongvybts13613@gmail.com>
2026-06-01 21:33:22 +1000

    Update tailwind.config.js
    change "module.export" to "export default"

--- frontend/config/tailwind.config.js
+++ frontend/config/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default = {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}", // Đảm bảo có dòng này để quét các file JSX
